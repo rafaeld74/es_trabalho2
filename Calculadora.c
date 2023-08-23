b1a46d043e5640684f42432d335ead56feea6d7f
rafaeld74 <129432560+rafaeld74@users.noreply.github.com>
2023-08-23 02:57:43 -0300

    feat: adicionando a funcao de octal

--- Calculadora.c
+++ Calculadora.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-
+// Função para converter um número decimal para binário
 void decimalParaBinario(int numero) {
-    int binario[32];
+    int binario[32]; // Um número inteiro em C é geralmente representado em 32 bits
     int i = 0;
 
     if (numero == 0) {
@@ -23,6 +23,29 @@ void decimalParaBinario(int numero) {
     printf("\n");
 }
 
+// Função para converter um número decimal para octal
+void decimalParaOctal(int numero) {
+    int octal[32]; // Um número inteiro em C é geralmente representado em 32 bits
+    int i = 0;
+
+    if (numero == 0) {
+        printf("0 em octal é 0\n");
+        return;
+    }
+
+    while (numero > 0) {
+        octal[i] = numero % 8;
+        numero = numero / 8;
+        i++;
+    }
+
+    printf("%d em octal é: ", numero);
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", octal[j]);
+    }
+    printf("\n");
+}
+
 int main() {
     int numero;
 
@@ -30,6 +53,7 @@ int main() {
     scanf("%d", &numero);
 
     decimalParaBinario(numero);
+    decimalParaOctal(numero);
 
     return 0;
 }
